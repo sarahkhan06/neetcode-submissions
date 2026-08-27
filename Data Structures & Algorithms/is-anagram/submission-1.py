d170d5d658dc189b12798dab455514409ce88e77
Sarah Khan <119809135+sarahkhan06@users.noreply.github.com>
2026-08-27 17:18:27 -0500

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #return Counter(s) == Counter(t)
+        if len(s) != len(t):
+           return False
+        return sorted(s) == sorted(t)
+        
